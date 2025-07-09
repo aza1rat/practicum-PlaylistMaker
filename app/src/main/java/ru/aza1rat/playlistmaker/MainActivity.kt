12a9bfeba9a50fde03a468cc655ed6aeea2b29ca
aza1rat <artemkashityn193@gmail.com>
2025-07-09 13:19:55 +0300

    sprint 8 task 1 & 2

--- app/src/main/java/ru/aza1rat/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/aza1rat/playlistmaker/MainActivity.kt
@@ -1,14 +1,31 @@
 package ru.aza1rat.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search = findViewById<MaterialButton>(R.id.search)
+        val mediaLibrary = findViewById<MaterialButton>(R.id.media_library)
+        val settings = findViewById<MaterialButton>(R.id.settings)
+        search.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity,"Настройки | Через анонимный класс",
+                    Toast.LENGTH_LONG).show()
+            }
+        })
+        mediaLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Медиатека | Через лямбду",
+                Toast.LENGTH_LONG).show()
+        }
+        settings.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Настройки | Через лямбду",
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
